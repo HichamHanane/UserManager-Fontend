1bb70c13ed0b427d4ec156ef5a9c692b2e30a491
HichamHanane <hichamhanane2000@gmail.com>
2025-08-26 03:58:46 +0200

    create login page

---
+++ src/pages/loginPage/LoginPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './LoginPage.css'
+import Login from '../../components/Login/Login'
+function LoginPage() {
+  return (
+    <div className='loginpage'>
+      <Login />
+    </div>
+  )
+}
+
+export default LoginPage
